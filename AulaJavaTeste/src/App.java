23f35a661493402440665924c53571dcf2e2e582
Mateus Haccourt Woikolesko <142453310+deepmatador@users.noreply.github.com>
2024-04-02 20:52:24 -0300

    Versao 1

--- AulaJavaTeste/src/App.java
+++ AulaJavaTeste/src/App.java
@@ -2,6 +2,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
+        
+        System.out.println("Temos que sair correndo daqui");
+
+        int mateus = 10;
+
+        System.out.println(mateus);
+
+
+    
+
         System.out.println("Mateus é um pé no saco");
 
 
