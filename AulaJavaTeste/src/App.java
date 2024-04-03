f2eee085b775895a36aeedd33e19fc628aef6cd1
Mateus Haccourt Woikolesko <142453310+deepmatador@users.noreply.github.com>
2024-04-02 21:24:07 -0300

    Aula dia 12
    Aula dia 12 aonde eu fiz a

--- AulaJavaTeste/src/App.java
+++ AulaJavaTeste/src/App.java
@@ -1,19 +1,10 @@
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        
-        System.out.println("Temos que sair correndo daqui");
-
-        int mateus = 10;
-
-        System.out.println(mateus);
-
-
-    
-
-        System.out.println("Mateus é um pé no saco");
-
 
 
     }
