a1c37b405e2639a9d2d243b61289c365b03f76aa
Mateus Haccourt Woikolesko <142453310+deepmatador@users.noreply.github.com>
2024-04-03 21:56:35 -0300

    Mateus

--- AulaJavaTeste/Mateus01/src/App.java
+++ AulaJavaTeste/Mateus01/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int numero_inteiro = 10;
+        int numeroInteiro = 10;
+        int num_inteiro = 10;
+
+        System.out.println();
+
+        System.out.println("*************LISTA DE NÚMEROS************");
+        System.out.println("Valor inicial: " + num_inteiro);
+        System.out.println("O Valor final: " + numero_inteiro);
+        System.out.println("Valor final é: " + numeroInteiro);
+        System.out.println("******************************************");
+
     }
 }
