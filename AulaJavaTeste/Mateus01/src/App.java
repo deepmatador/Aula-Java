7afe789e43f792dd643c9762e75d9042a72f6d9b
Mateus Haccourt Woikolesko <142453310+deepmatador@users.noreply.github.com>
2024-04-04 19:38:22 -0300

    Exclusão

--- AulaJavaTeste/Mateus01/src/App.java
+++ AulaJavaTeste/Mateus01/src/App.java
@@ -2,24 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        int numero_inteiro = 10;
-        int numeroInteiro = 10;
-        int num_inteiro = 10;
-
-        System.out.println();
-
-        System.out.println("*************LISTA DE NÚMEROS************");
-        System.out.println("Valor inicial: " + num_inteiro);
-        System.out.println("O Valor final: " + numero_inteiro);
-        System.out.println("Valor final é: " + numeroInteiro);
-        System.out.println("******************************************");
-        System.out.println("MEu nome é Mateus Deep");
-      
-        System.out.println("Esse é um teste realizado do meu computador da TV Nova Conexão");
-        System.out.println("Teste das 09:04");
-
-    System.out.println("Mateus é um gostoso safado");
-    
+        
         
     }
 }
