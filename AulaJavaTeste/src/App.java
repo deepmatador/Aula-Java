ff6c1d6a0d43e04875e356f4141c05c940607848
Mateus Haccourt Woikolesko <142453310+deepmatador@users.noreply.github.com>
2024-04-03 21:14:26 -0300

    Update Java

--- AulaJavaTeste/src/App.java
+++ AulaJavaTeste/src/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         System.out.println("Hello, World!");
-
+        System.out.println("asodkjsaopdkosapkdokasod");
 
     }
 }
