c19520f903205ed4393c5164aefba98230843380
Mateus <deepfotografias@hotmail.com>
2024-04-08 19:58:44 -0300

    Commitando

---
+++ AulaJavaTeste/App2/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String provedor = "Deep Web S.A";
+        String atendente = "Vanessinha da Mata";
+
+
+
+        System.out.printf("Olá, bem-vindo a central de atendimento %s", provedor);
+
+        System.out.println("Meu nome é {atendente}, sou seu assistente virtual");
+
+        System.out.println("Durante o atendimento siga minhas instruções");
+
+        System.out.println("Primeiramente informe seu nome");
+
+        System.out.println("Obrigado {nome}, como posso lhe ajudar?");
+
+        System.out.println("Hello, World!");
+    }
+}
