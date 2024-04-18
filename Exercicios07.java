deecfafb43945bd4eeff0215ad6c2892b0b2c810
Mateus <deepfotografias@hotmail.com>
2024-04-18 20:37:13 -0300

    Commit feito dia 18/04

--- Exercicios07.java
+++ Exercicios07.java
@@ -2,25 +2,22 @@ import java.util.Scanner;
 
 public class Exercicios07 {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Insira uma caractere alfabético para saber se ele é uma vogal ou uma consoante");
         var caractere = scanner.next().charAt(0);
 
-       // caractere = Character.toLowerCase(caractere);
+        // caractere = Character.toLowerCase(caractere);
 
-        if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u'|| caractere == 'A' || caractere == 'E' || caractere == 'I' ||caractere == 'O' || caractere == 'U') {
+        if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u'
+                || caractere == 'A' || caractere == 'E' || caractere == 'I' || caractere == 'O' || caractere == 'U') {
             System.out.println("Seu caractere é uma VOGAL");
         } else {
             System.out.println("O caractere inserido é CONSOANTE");
         }
 
-
-
-        
-
         scanner.close();
     }
-    
+
 }
--- Exercicios08.java
+++ Exercicios08.java
@@ -15,6 +15,6 @@ public static void main(String[] args) {
     }
    
     scanner.close();
-}
+   }
 
 }
